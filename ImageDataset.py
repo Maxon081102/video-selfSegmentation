e0ff05d33b628560be2913c435c73f03cea5adf5
Maxon081102 <nikikitinm117@gmai.com>
2023-04-20 19:14:01 +0300

    add notebook with model training

--- ImageDataset.py
+++ ImageDataset.py
@@ -1,14 +1,17 @@
 import os
+import torch
+import numpy as np
+
 from torch.utils.data import Dataset, DataLoader
 from tqdm.notebook import tqdm
 from imgPrepare import img_prepare
+
 class ImageDataset(Dataset):
-    def __init__(self, data_folder, img_prepare, transform):
+    def __init__(self, img_names, img_prepare, transform):
         self.data = []
-        for img_name in tqdm(os.listdir(data_folder + "/X")):
-            if os.path.isfile(data_folder + "/Y/" + img_name.replace("X", "Y")):
-                img, mask = img_prepare(img_name, transform)
-                self.data.append((img, mask))
+        for img_name in tqdm(img_names):
+            img, mask = img_prepare(img_name, transform)
+            self.data.append((img, mask))
 
     def __getitem__(self, index):
         img, mask = self.data[index]
@@ -16,6 +19,20 @@ class ImageDataset(Dataset):
 
     def __len__(self):
         return len(self.data)
+
+def get_img_for_train_and_val(data_folder, p=0.7, seed=777, img_count=100000):
+    train = []
+    test = []
+    np.random.seed(seed)
+    for img_name in os.listdir(data_folder + "/X"):
+        if os.path.isfile(data_folder + "/Y/" + img_name.replace("X", "Y")):
+            if np.random.rand() >= p:
+                if len(test) < img_count:
+                    test.append(img_name)
+            else:
+                if len(train) < img_count:
+                    train.append(img_name)
+    return train, test
 
 def collate_fn(batch):
     img_batch = torch.stack([elem[0] for elem in batch])
